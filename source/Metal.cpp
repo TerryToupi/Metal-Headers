0e98275cebf24bba350f4df0b337079a76d65b8b
TerryToupi <terrytoupi@gmail.com>
2025-08-11 14:31:56 +0300

    removed single header

--- source/Metal.cpp
+++ source/Metal.cpp
@@ -1,4 +1,6 @@
 #define NS_PRIVATE_IMPLEMENTATION
 #define CA_PRIVATE_IMPLEMENTATION
 #define MTL_PRIVATE_IMPLEMENTATION
-#include <Metal.hpp>
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
